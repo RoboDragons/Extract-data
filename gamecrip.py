673d314c334e90a8f917bcc31a57cfcd4b154774
tomoakiwakawaka <tomoakiwakayama0821@gmail.com>
2025-01-12 13:46:17 +0900

    バールの位置を分析

--- gamecrip.py
+++ gamecrip.py
@@ -115,7 +115,6 @@ def main():
                     if ball and ball.x > 0 and attacking_blue_robots_count > total_blue_robots / 2:
                         offense_frames.append(pside_offense_robo_position)
                     
-                    
 
             except Exception as e:
                 print(f"Error while processing data: {e}")
---
+++ plotxball.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# データの読み込み
+dataframe = pd.read_csv('./out/ssl-vision-client.csv')
+dataframe.head()
+
+# ヒストグラムのプロット
+dataframe['x'].hist(bins=20)  # 基数の数を20個にする
+
+plt.hist(dataframe['x'].dropna(), bins=10, range=(-6000, 6000), color='Blue')
+plt.xlabel('X Position')
+plt.ylabel('Frequency')
+plt.title('Histogram of X Position')
+plt.show()
