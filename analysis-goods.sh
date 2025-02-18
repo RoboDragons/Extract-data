1da71a9beac87266f705377121773ae75f09fec9
Manato-310 <131521917+Manato-310@users.noreply.github.com>
2025-02-18 14:28:55 +0900

    analysis-goods.sh 改訂版

--- analysis-goods.sh
+++ analysis-goods.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
 
-# Clone the SSL-Vision repository
-
+# visionとlogtoolsをclone
+git clone git@github.com:RoboCup-SSL/ssl-vision.git
 git clone git@github.com:RoboCup-SSL/ssl-logtools.git
-cd ssl-logtools
 
-# Create and move to the build directory
+# Qt4とBoostをインストール
+sudo apt update
+sudo apt install qt4-default
+sudo apt install libboost-all-dev
+
+# ssl-logtoolsの準備
+cd ssl-logtools
 mkdir build
 cd build
-
-# Run CMake and Make
 cmake ..
 make
 
-# Install cmake if it's missing
-sudo apt-get install cmake
-
-# Remove potential lock files
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/lib/dpkg/lock-frontend
-
-# Install cmake again just to be sure
-sudo apt-get install cmake
-
-# Run make again
+# ssl-visionの準備
+cd ~/ssl-vision
+./InstallPackagesUbuntu.sh
 make
-
-# Add the PPA and install QT4 packages
-sudo add-apt-repository ppa:/ubuntuhandbook1/ppa
-sudo apt install qt4-*
-
-# Run make again
-make
-
-# Clone the SSL-Logtools repository
-cd ../../
-git clone git@github.com:RoboCup-SSL/ssl-vision.git
-cd ./ssl-vision
-
-# Run the Install Packages script
-sudo ./InstallPackagesUbuntu.sh
