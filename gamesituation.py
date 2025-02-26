f59ab88402323f67a90c8d72180564fd804e032a
tomoakiwakawaka <tomoakiwakayama0821@gmail.com>
2025-02-26 15:29:24 +0900

    Revert "Add files via upload"
    This reverts commit 7a8d309f4881303038b15799f4c3b117222375cc.

--- gamesituation.py
+++ gamesituation.py
@@ -6,26 +6,32 @@ import numpy as np
 import pandas as pd
 import time
 import messages_robocup_ssl_wrapper_pb2
+import ssl_gc_state_pb2
+import ssl_gc_engine_pb2
 import ssl_gc_referee_message_pb2
 import math
 
 debug = False
-stop_event = threading.Event()
-
-game_state = None  # グローバル変数としてゲームの状態を管理
-game_time = 0.0
-blue_possession_time = 0.0
-yellow_possession_time = 0.0
-Game_on = [2, 3, 4, 5, 8, 9]
-
+STATE_LIST = []
+State_num = 0
+count = 0
 sock = None
+Game_on = [2, 3, 4, 5, 8, 9]
+stop_event = threading.Event()
+game_time=0.0
+blue_possession_time=0.0
+yellow_possession_time=0.0
 
 def setup_socket():
     global sock
+    buffer_size = 4096  # バッファサイズ データの受け取るお皿の大きさ
     multicast_group = '224.5.23.1'
-    server_address = ('', 10003)
+    server_address = ('', 10003)  # GCのアドレス データが送られてくる元の
 
+    # ソケットの作成 SOCK_DGRAMはUDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # ソケットにオプションを設定し、マルチキャストグループに参加
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(server_address)
 
@@ -34,28 +40,94 @@ def setup_socket():
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 def receive_game_controller_signal():
-    """ ゲームコントローラの信号を受信し、状態をグローバル変数に格納する """
-    global game_state
-    buffer_size = 4096
+    global sock
+    buffer_size = 4096  # バッファサイズ データの受け取るお皿の大きさ
 
-    while not stop_event.is_set():
-        try:
-            data, _ = sock.recvfrom(buffer_size)
-            referee_message = ssl_gc_referee_message_pb2.Referee()
-            referee_message.ParseFromString(data)
-            game_state = referee_message.command  # 最新のゲーム状態を更新
-        except Exception as e:
-            print("RefereeMessage デコードエラー:", e)
+    try:
+        while not stop_event.is_set():
+            data, address = sock.recvfrom(buffer_size)  # データ受信
+            if debug:
+                print("データを受信しました from", address)
+                print("受信データ (バイナリ):", data)  # 受信データをバイナリ形式で表示
+
+            try:
+                # RefereeMessageのデコード
+                referee_message = ssl_gc_referee_message_pb2.Referee()
+                referee_message.ParseFromString(data)
+                if debug:
+                    print("Referee Message (人間が読みやすい形式):")
+
+                #print(referee_message.Command.Name(referee_message.command))
+                return referee_message.command
+            except Exception as e:
+                print("RefereeMessage デコードエラー:", e)
+                print("----------------------")
+
+    except KeyboardInterrupt:
+        print("終了処理を開始します...")
 
 def track_ball_position():
-    """ ボールの位置を記録する """
     local = "127.0.0.1"
     multicast = "224.5.23.2"
     port = 10006
     buffer = 65536
 
     path = "out/"
-    ballpojiPath = os.path.join(path, "ballpoji.csv")
+    ballpojiPath = path + "ballpoji.csv"
+
+    addr = ('', port)
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udp.bind(addr)
+    udp.setsockopt(socket.IPPROTO_IP,
+                   socket.IP_ADD_MEMBERSHIP,
+                   socket.inet_aton(multicast) + socket.inet_aton(local))
+
+    ball_position = []
+
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    while not stop_event.is_set():
+        try:
+            packet = messages_robocup_ssl_wrapper_pb2.SSL_WrapperPacket()
+            data, _ = udp.recvfrom(buffer)
+            packet.ParseFromString(data)
+
+            frame = packet.detection
+            if debug:
+                print("frame: ", frame)
+            if frame:
+                balls = frame.balls
+                if balls:
+                    ball = balls[0]
+                    
+                    if receive_game_controller_signal() in Game_on:
+                        #print("here")
+                        if frame.frame_number % 10 == 0:
+                            balls_position = [int(ball.x) / 10, int(ball.y) / 10, receive_game_controller_signal()]
+                            ball_position.append(balls_position)
+                            df = pd.DataFrame(ball_position, columns=["x", "y", "state"])
+                            df = df.sort_values("x")
+                            df.to_csv(ballpojiPath, header=True, index=False)
+
+                        if debug:
+                            print("ball_position: ", ball_position)
+                            print("\n")
+        except KeyboardInterrupt:
+            break
+
+    udp.close()
+
+def possession():
+    global game_time, blue_possession_time, yellow_possession_time  # グローバル変数として宣言
+    local = "127.0.0.1"
+    multicast = "224.5.23.2"
+    port = 10006
+    buffer = 65536
+
+    path = "out/"
+    possessionPath = path + "possession.csv"
 
     addr = ('', port)
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -68,8 +140,21 @@ def track_ball_position():
     if not os.path.isdir(path):
         os.mkdir(path)
 
-    ball_position = []
 
+    ball_to_bluerobots_distance=[]
+    ball_to_yellowrobots_distance=[]    
+    ball_to_bluerobots_id=[]
+    ball_to_yellowrobots_id=[]
+    min_dist_blue_robot_index=0
+    min_dist_yellow_robot_index=0
+    holding_num=[]
+    ball_holding_robot=[]
+    
+    robots_radius=800
+    holding_distance=15000
+    
+    
+    
     while not stop_event.is_set():
         try:
             packet = messages_robocup_ssl_wrapper_pb2.SSL_WrapperPacket()
@@ -77,41 +162,90 @@ def track_ball_position():
             packet.ParseFromString(data)
 
             frame = packet.detection
-            if frame and game_state in Game_on:  # ゲームが進行中かチェック
-                balls = frame.balls
-                if balls:
-                    ball = balls[0]
-                    if frame.frame_number % 10 == 0:
-                        balls_position = [int(ball.x), int(ball.y), game_state]
-                        ball_position.append(balls_position)
-
-                        # CSVに書き込み
-                        df = pd.DataFrame(ball_position, columns=["x", "y", "state"])
-                        df.to_csv(ballpojiPath, header=True, index=False)
-
+            if receive_game_controller_signal() in Game_on:
+                game_time+=1.0
+                if frame:
+                    #print("frame: ", frame)
+                    robots_blue = frame.robots_blue
+                    robots_yellow = frame.robots_yellow
+                    balls = frame.balls
+                    #print(robots_blue)
+                    if balls:
+                        ball=balls[0]
                         if debug:
-                            print("ball_position: ", balls_position)
+                            print("ball: ", int(ball.x))
+                        if robots_blue:
+                            for i in range(len(robots_blue)):
+                                ball_to_bluerobots_distance.append((robots_blue[i].x-ball.x)**2+(robots_blue[i].y-ball.y)**2)
+                                ball_to_bluerobots_id.append(robots_blue[i].robot_id)
+                            if ball_to_bluerobots_distance:
+                                min_dist_blue_robot_index=ball_to_bluerobots_distance.index(min(ball_to_bluerobots_distance))
+                                
+                            if 0 <= min_dist_blue_robot_index < len(robots_blue):
+                                if math.atan(robots_blue[min_dist_blue_robot_index].orientation)*(ball.x-robots_blue[min_dist_blue_robot_index].x)+robots_blue[min_dist_blue_robot_index].y+robots_radius > ball.y and math.atan(robots_blue[min_dist_blue_robot_index].orientation)*(ball.x-robots_blue[min_dist_blue_robot_index].x)+robots_blue[min_dist_blue_robot_index].y-robots_radius < ball.y:
+                                    if min(ball_to_bluerobots_distance) < holding_distance*holding_distance:
+                                        blue_possession_time+=1.0
+                                        holding_num.append(robots_blue[min_dist_blue_robot_index].robot_id)
+                                        if holding_num.count(robots_blue[min_dist_blue_robot_index].robot_id) >=10:
+                                            print("blue",min_dist_blue_robot_index)
+                                            ball_holding_robot.append([robots_blue[min_dist_blue_robot_index].robot_id,balls[0].x,balls[0].y,blue_possession_time,yellow_possession_time,game_time])
+                                            holding_num.clear()
+                            min_dist_blue_robot_index=-1
+                            ball_to_bluerobots_distance.clear()
+                            ball_to_bluerobots_id.clear()
+                            
+                        if robots_yellow:
+                            for i in range(len(robots_yellow)):
+                                ball_to_yellowrobots_distance.append((robots_yellow[i].x-ball.x)**2+(robots_yellow[i].y-ball.y)**2)
+                                ball_to_yellowrobots_id.append(robots_yellow[i].robot_id)
+                            if ball_to_yellowrobots_distance:
+                                min_dist_yellow_robot_index=ball_to_yellowrobots_distance.index(min(ball_to_yellowrobots_distance))
+                            if 0 <= min_dist_yellow_robot_index < len(robots_yellow):
+                                if math.atan(robots_yellow[min_dist_yellow_robot_index].orientation)*(ball.x-robots_yellow[min_dist_yellow_robot_index].x)+robots_yellow[min_dist_yellow_robot_index].y+robots_radius > ball.y and math.atan(robots_yellow[min_dist_yellow_robot_index].orientation)*(ball.x-robots_yellow[min_dist_yellow_robot_index].x)+robots_yellow[min_dist_yellow_robot_index].y-robots_radius < ball.y:
+                                    if min(ball_to_yellowrobots_distance) < holding_distance*holding_distance:
+                                        yellow_possession_time+=1.0
+                                        holding_num.append(robots_yellow[min_dist_yellow_robot_index].robot_id)
+                                        if holding_num.count(robots_yellow[min_dist_yellow_robot_index].robot_id) >=10:
+                                            print("yellow",min_dist_yellow_robot_index)
+                                            ball_holding_robot.append([robots_yellow[min_dist_yellow_robot_index].robot_id,balls[0].x,balls[0].y,blue_possession_time,yellow_possession_time,game_time])
+                                            holding_num.clear()
+                        min_dist_yellow_robot_index=-1
+                        ball_to_yellowrobots_distance.clear()
+                        ball_to_yellowrobots_id.clear()
+                    
+                                            
+                df = pd.DataFrame(ball_holding_robot, columns=["robot_id","ball_holding_place_x" ,"ball_holding_place_y","blue_possession_time", "yellow_possession_time", "game_time"])
+                df = df.sort_values("game_time")
+                df.to_csv(possessionPath, header=True, index=False)
+        except KeyboardInterrupt:
+            break
         except Exception as e:
-            print("ボール位置デコードエラー:", e)
+                print("RefereeMessage デコードエラー:", e)
+                print("b=",min_dist_blue_robot_index)
+                print("y=",min_dist_yellow_robot_index)
 
     udp.close()
 
 if __name__ == "__main__":
     setup_socket()
-
+    # スレッドを作成して、両方の関数を並行して実行
     thread1 = threading.Thread(target=receive_game_controller_signal)
     thread2 = threading.Thread(target=track_ball_position)
+    thread3 = threading.Thread(target=possession)
 
     thread1.start()
     thread2.start()
+    thread3.start()
 
     try:
         thread1.join()
         thread2.join()
+        thread3.join()
     except KeyboardInterrupt:
         stop_event.set()
         thread1.join()
         thread2.join()
+        thread3.join()
 
     sock.close()
     print("ソケットを閉じました")
