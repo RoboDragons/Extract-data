109ba07b8446cdec2672ca9e718ed67a2d55813b
PCwakayama <tomoakiwakayama0821@gmail.com>
2025-03-23 13:53:37 +0900

    コメント追加

--- analysis-goods.sh
+++ analysis-goods.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
 # Qt4とBoostをインストール
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install qt4-default
-sudo apt install libboost-all-dev
+sudo apt update -y                # パッケージリストを最新に更新
+sudo apt upgrade -y               # システムのパッケージを最新に更新
+sudo apt install qt4-default      # Qt4（GUIアプリケーションフレームワーク）のインストール
+sudo apt install libboost-all-dev # C++ライブラリのBoostをインストール
 
 # Extract-dataをclone
-git clone git@github.com:RoboDragons/Extract-data.git
-cd ~/Extract-data
-git submodule update --init --recursive
+git clone git@github.com:RoboDragons/Extract-data.git  # Extract-dataリポジトリをGitHubからクローン
+cd ~/Extract-data                                      # プロジェクトフォルダに移動
+git submodule update --init --recursive                # サブモジュールも含めて最新化
 
 # ssl-logtoolsの準備
-cd ~/Extract-data/ssl-logtools
-mkdir build
-cd build
-cmake ..
-make
+cd ~/Extract-data/ssl-logtools    # ssl-logtoolsディレクトリに移動
+mkdir build                       # buildフォルダを作成
+cd build                          # buildフォルダに移動
+cmake ..                          # cmake でビルド環境を構築
+make                              # make でコンパイル
 
 # ssl-visionの準備
-cd ~/Extract-data/ssl-vision
-./InstallPackagesUbuntu.sh
-make
-cd ~
+cd ~/Extract-data/ssl-vision             # ssl-visionディレクトリへ移動
+./InstallPackagesUbuntu.sh               # 依存パッケージのインストール
+make                                     # ビルド
+cd ~                                     # ホームディレクトリに戻る
 
 # Python環境の構築
-sudo apt install python3 python3-pip -y
-sudo apt install python3-venv -y
-python3 -m venv ~/myenv
-source ~/myenv/bin/activate
-pip install numpy pandas matplotlib seaborn
-pip install protobuf==3.20.0
-sudo apt install python3-tk python3-dev -y
+sudo apt install python3 python3-pip -y  # Python3 と Pip（パッケージ管理ツール）をインストール
+sudo apt install python3-venv -y         # python3-venv をインストール（仮想環境の作成に必要）
+python3 -m venv ~/myenv                  # 仮想環境を作成
+#pythonの仮想環境を無効化するコマンド
+#deactivate
+source ~/myenv/bin/activate              # 仮想環境を有効化
+pip install numpy pandas matplotlib seaborn  # データ解析に必要なライブラリをインストール
+pip install protobuf==3.20.0                 # プロトコルバッファーのバージョン指定
+sudo apt install python3-tk python3-dev -y   # GUIツールキットと開発用ライブラリをインストール
 
 # Java環境の構築
-sudo apt install openjdk-21-jdk -y
+sudo apt install openjdk-21-jdk -y       # Java 21のJDKをインストール
 
 # ~/.bashrc のパスを指定
 BASHRC_FILE="$HOME/.bashrc"
@@ -46,12 +48,12 @@ if ! grep -q "cd" "$BASHRC_FILE"; then
 function cd() {
     builtin cd "$@" || return
     if [[ "$PWD" == *"/Extract-data" ]]; then
-        source ~/myenv/bin/activate
+        source ~/myenv/bin/activate    # Extract-dataディレクトリに移動すると、自動的に仮想環境が有効化
     fi
 }
 EOF
     echo "内容を~/.bashrc に追加しました。"
     bash
 else
-    echo "~/.bashrc に既に指定の内容があります。"
+    echo "~/.bashrc に既に指定の内容があります。"  # 重複を避けるためのチェック
 fi
