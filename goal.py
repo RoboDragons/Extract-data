4bfb7987f3eb47499f1a937fdef01adc95b23cf4
kusakabe <monarda.k.4470@gmail.com>
2025-08-21 03:03:59 +0900

    条件と変数名の修正

--- goal.py
+++ goal.py
@@ -6,8 +6,8 @@ def goal_scene(track_ball_position, track_robot_position, path, stop_event, debu
     robot_position_path = os.path.join(path, "robot_position.csv") # ロボットのポジションのCSVファイルパス
     poji_goal_x=6000 # 右側ゴールのX座標
     nega_goal_x=-6000 # 左側ゴールのX座標
-    poji_goal_y=600 # 右側ゴールのY座標
-    nega_goal_y=600 # 左側ゴールのY座標
+    goal_top=900 # ゴールの上端
+    goal_bottom=-900 # ゴールの下端
     if not os.path.isdir(path):
         os.mkdir(path) # ディレクトリが存在しない場合は作成
     while not stop_event.is_set(): # スレッドが停止されるまでループ
@@ -17,7 +17,7 @@ def goal_scene(track_ball_position, track_robot_position, path, stop_event, debu
             if balls_position and robot_poji: 
                 ball_x, ball_y, state = balls_position[-1] # ボールの位置の最後の要素を取得
                 if ball_x > 6000:
-                    if robot_poji and ((ball_x > poji_goal_x and (poji_goal_y > ball_y > nega_goal_y)) or (ball_x < nega_goal_x and (poji_goal_y > ball_y > nega_goal_y))): # ゴールシーンの条件
+                    if robot_poji and ((ball_x > poji_goal_x and (goal_top > ball_y > goal_bottom)) or (ball_x < nega_goal_x and (goal_top > ball_y > goal_bottom))): # ゴールシーンの条件
                         df_robot_position = pd.read_csv(robot_position_path) # ロボットのポジションのCSVファイルを読み込み
                         last_20_frames = df_robot_position.tail(120) # 最後の120フレームを取得
                         df = pd.DataFrame(last_20_frames) # DataFrameを作成
